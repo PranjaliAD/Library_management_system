71d130cb734ec5af88cee23e56a008aab4cafd6b
PranjaliAD <pranjalidharmadhikari2002@gmail.com>
2024-09-27 00:31:38 +0530

    Add test for borrowing books

--- library.test.js
+++ library.test.js
@@ -8,3 +8,19 @@ test('should add a book to the library', () => {
 
   expect(library.getAvailableBooks()).toContain(book);
 });
+
+test('should borrow a book from the library', () => {
+    const library = new Library();
+    const book = new Book('1234', 'The Clean Coder', 'Robert C. Martin', 2011);
+    library.addBook(book);
+  
+    library.borrowBook('1234');
+    expect(library.getAvailableBooks()).not.toContain(book);
+});
+  
+test('should raise an error when borrowing an unavailable book', () => {
+    const library = new Library();
+  
+    expect(() => library.borrowBook('999')).toThrow('Book not available');
+});
+  
