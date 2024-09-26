a6644493ed60db6f0c72aabf0bea00c1aea97848
PranjaliAD <pranjalidharmadhikari2002@gmail.com>
2024-09-27 00:34:15 +0530

    Implement borrow book functionality

--- library.js
+++ library.js
@@ -10,6 +10,16 @@ class Library {
     getAvailableBooks() {
       return this.books;
     }
+
+    borrowBook(isbn) {
+        const bookIndex = this.books.findIndex(book => book.isbn === isbn);
+        if (bookIndex !== -1) {
+          this.books.splice(bookIndex, 1); // Remove the book
+        } 
+        else {
+          throw new Error('Book not available');
+        }
+    }
   }
   
 module.exports = Library;
